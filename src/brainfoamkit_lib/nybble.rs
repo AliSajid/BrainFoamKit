a877fe090007a7bcbfaa155a4e3f01dfac923cd6
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-08-08 22:24:36 -0400

    refactor(Nybble): refactor Nybble and add more tests

--- src/brainfoamkit_lib/nybble.rs
+++ src/brainfoamkit_lib/nybble.rs
@@ -45,7 +45,7 @@
 use crate::Bit;
 use std::{
     fmt::{self, Display, Formatter},
-    ops::Not,
+    ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Not},
 };
 
 /// A Nybble is a 4-bit unsigned integer (u4).
@@ -131,30 +131,28 @@ impl Nybble {
     /// ```
     ///
     #[must_use]
-    pub fn from_u8(n: u8) -> Option<Self> {
-        // Check if the input value is within the valid range for a Nybble (0-15)
-        if n <= 15 {
-            // Create a new Nybble instance with default Bit values
-            let mut nybble = Self::default();
+    pub fn from_u8(n: u8) -> Self {
+        // Use only the first four bits of the input value if it is larger than 15
+        let n = if n > 15 { n & 0b1111 } else { n };
 
-            // Test each bit in the u8 value and flip the corresponding bit in the Nybble if necessary
-            if n & 0b0001 != 0 {
-                nybble.bit_0.flip();
-            };
-            if n & 0b0010 != 0 {
-                nybble.bit_1.flip();
-            };
-            if n & 0b0100 != 0 {
-                nybble.bit_2.flip();
-            };
-            if n & 0b1000 != 0 {
-                nybble.bit_3.flip();
-            };
+        // Create a new Nybble instance with default Bit values
+        let mut nybble = Self::default();
 
-            Some(nybble)
-        } else {
-            None // Return None if the input value is out of range
-        }
+        // Test each bit in the u8 value and flip the corresponding bit in the Nybble if necessary
+        if n & 0b0001 != 0 {
+            nybble.bit_0.set();
+        };
+        if n & 0b0010 != 0 {
+            nybble.bit_1.set();
+        };
+        if n & 0b0100 != 0 {
+            nybble.bit_2.set();
+        };
+        if n & 0b1000 != 0 {
+            nybble.bit_3.set();
+        };
+
+        nybble
     }
 
     /// Sets the Bit value at the specified index.
@@ -179,10 +177,10 @@ impl Nybble {
     /// This method will panic if the index is out of bounds.
     pub fn set_bit(&mut self, index: usize) {
         match index {
-            0 => self.bit_0 = Bit::one(),
-            1 => self.bit_1 = Bit::one(),
-            2 => self.bit_2 = Bit::one(),
-            3 => self.bit_3 = Bit::one(),
+            0 => self.bit_0.set(),
+            1 => self.bit_1.set(),
+            2 => self.bit_2.set(),
+            3 => self.bit_3.set(),
             _ => panic!("Index out of bounds"),
         }
     }
@@ -212,10 +210,10 @@ impl Nybble {
     /// This method will panic if the index is out of bounds.
     pub fn unset_bit(&mut self, index: usize) {
         match index {
-            0 => self.bit_0 = Bit::zero(),
-            1 => self.bit_1 = Bit::zero(),
-            2 => self.bit_2 = Bit::zero(),
-            3 => self.bit_3 = Bit::zero(),
+            0 => self.bit_0.unset(),
+            1 => self.bit_1.unset(),
+            2 => self.bit_2.unset(),
+            3 => self.bit_3.unset(),
             _ => panic!("Index out of bounds"),
         }
     }
@@ -232,21 +230,13 @@ impl Nybble {
     /// let result = nybble.to_u8();
     /// assert_eq!(result, 5);
     /// ```
-    #[must_use]
     pub fn to_u8(&self) -> u8 {
         let mut n = 0;
 
-        if self.get_bit(3) == Bit::One {
-            n |= 8; // Set the fourth least significant bit of n to 1
-        }
-        if self.get_bit(2) == Bit::One {
-            n |= 4; // Set the third least significant bit of n to 1
-        }
-        if self.get_bit(1) == Bit::One {
-            n |= 2; // Set the second least significant bit of n to 1
-        }
-        if self.get_bit(0) == Bit::One {
-            n |= 1; // Set the least significant bit of n to 1
+        for i in 0..4 {
+            if self.get_bit(i) == Bit::One {
+                n |= 1 << i;
+            }
         }
 
         n
@@ -376,146 +366,103 @@ impl Not for Nybble {
     }
 }
 
+impl BitAnd for Nybble {
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        let mut nybble = self;
+        nybble.bit_0 &= rhs.bit_0;
+        nybble.bit_1 &= rhs.bit_1;
+        nybble.bit_2 &= rhs.bit_2;
+        nybble.bit_3 &= rhs.bit_3;
+        nybble
+    }
+}
+
+impl BitAndAssign for Nybble {
+    fn bitand_assign(&mut self, rhs: Self) {
+        self.bit_0 &= rhs.bit_0;
+        self.bit_1 &= rhs.bit_1;
+        self.bit_2 &= rhs.bit_2;
+        self.bit_3 &= rhs.bit_3;
+    }
+}
+
+impl BitOr for Nybble {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        let mut nybble = self;
+        nybble.bit_0 |= rhs.bit_0;
+        nybble.bit_1 |= rhs.bit_1;
+        nybble.bit_2 |= rhs.bit_2;
+        nybble.bit_3 |= rhs.bit_3;
+        nybble
+    }
+}
+
+impl BitOrAssign for Nybble {
+    fn bitor_assign(&mut self, rhs: Self) {
+        self.bit_0 |= rhs.bit_0;
+        self.bit_1 |= rhs.bit_1;
+        self.bit_2 |= rhs.bit_2;
+        self.bit_3 |= rhs.bit_3;
+    }
+}
+
+impl BitXor for Nybble {
+    type Output = Self;
+
+    fn bitxor(self, rhs: Self) -> Self::Output {
+        let mut nybble = self;
+        nybble.bit_0 ^= rhs.bit_0;
+        nybble.bit_1 ^= rhs.bit_1;
+        nybble.bit_2 ^= rhs.bit_2;
+        nybble.bit_3 ^= rhs.bit_3;
+        nybble
+    }
+}
+
+impl BitXorAssign for Nybble {
+    fn bitxor_assign(&mut self, rhs: Self) {
+        self.bit_0 ^= rhs.bit_0;
+        self.bit_1 ^= rhs.bit_1;
+        self.bit_2 ^= rhs.bit_2;
+        self.bit_3 ^= rhs.bit_3;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_nybble_zero() {
-        let nybble = Nybble::default();
+    fn test_from_u8() {
+        let nybble = Nybble::from_u8(10);
+        assert_eq!(nybble.to_u8(), 0b1010);
+    }
+
+    #[test]
+    fn test_from_u8_zero() {
+        let nybble = Nybble::from_u8(0);
         assert_eq!(nybble.to_u8(), 0);
-        assert_eq!(nybble.to_string(), "0x00");
-        assert_eq!(Nybble::from_u8(0), Some(nybble));
     }
 
     #[test]
-    fn test_nybble_one() {
-        let nybble = Nybble::from_u8(1).unwrap();
-        assert_eq!(nybble.to_u8(), 1);
-        assert_eq!(nybble.to_string(), "0x01");
-        assert_eq!(Nybble::from_u8(1), Some(nybble));
+    fn test_from_u8_all_ones() {
+        let nybble = Nybble::from_u8(0b1111);
+        assert_eq!(nybble.to_u8(), 0b1111);
     }
 
     #[test]
-    fn test_nybble_two() {
-        let nybble = Nybble::from_u8(2).unwrap();
-        assert_eq!(nybble.to_u8(), 2);
-        assert_eq!(nybble.to_string(), "0x02");
-        assert_eq!(Nybble::from_u8(2), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_three() {
-        let nybble = Nybble::from_u8(3).unwrap();
-        assert_eq!(nybble.to_u8(), 3);
-        assert_eq!(nybble.to_string(), "0x03");
-        assert_eq!(Nybble::from_u8(3), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_four() {
-        let nybble = Nybble::from_u8(4).unwrap();
-        assert_eq!(nybble.to_u8(), 4);
-        assert_eq!(nybble.to_string(), "0x04");
-        assert_eq!(Nybble::from_u8(4), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_five() {
-        let nybble = Nybble::from_u8(5).unwrap();
-        assert_eq!(nybble.to_u8(), 5);
-        assert_eq!(nybble.to_string(), "0x05");
-        assert_eq!(Nybble::from_u8(5), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_six() {
-        let nybble = Nybble::from_u8(6).unwrap();
-        assert_eq!(nybble.to_u8(), 6);
-        assert_eq!(nybble.to_string(), "0x06");
-        assert_eq!(Nybble::from_u8(6), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_seven() {
-        let nybble = Nybble::from_u8(7).unwrap();
-        assert_eq!(nybble.to_u8(), 7);
-        assert_eq!(nybble.to_string(), "0x07");
-        assert_eq!(Nybble::from_u8(7), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_eight() {
-        let nybble = Nybble::from_u8(8).unwrap();
-        assert_eq!(nybble.to_u8(), 8);
-        assert_eq!(nybble.to_string(), "0x08");
-        assert_eq!(Nybble::from_u8(8), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_nine() {
-        let nybble = Nybble::from_u8(9).unwrap();
-        assert_eq!(nybble.to_u8(), 9);
-        assert_eq!(nybble.to_string(), "0x09");
-        assert_eq!(Nybble::from_u8(9), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_ten() {
-        let nybble = Nybble::from_u8(10).unwrap();
-        assert_eq!(nybble.to_u8(), 10);
-        assert_eq!(nybble.to_string(), "0x0A");
-        assert_eq!(Nybble::from_u8(10), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_eleven() {
-        let nybble = Nybble::from_u8(11).unwrap();
-        assert_eq!(nybble.to_u8(), 11);
-        assert_eq!(nybble.to_string(), "0x0B");
-        assert_eq!(Nybble::from_u8(11), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_twelve() {
-        let nybble = Nybble::from_u8(12).unwrap();
-        assert_eq!(nybble.to_u8(), 12);
-        assert_eq!(nybble.to_string(), "0x0C");
-        assert_eq!(Nybble::from_u8(12), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_thirteen() {
-        let nybble = Nybble::from_u8(13).unwrap();
-        assert_eq!(nybble.to_u8(), 13);
-        assert_eq!(nybble.to_string(), "0x0D");
-        assert_eq!(Nybble::from_u8(13), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_fourteen() {
-        let nybble = Nybble::from_u8(14).unwrap();
-        assert_eq!(nybble.to_u8(), 14);
-        assert_eq!(nybble.to_string(), "0x0E");
-        assert_eq!(Nybble::from_u8(14), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_fifteen() {
-        let nybble = Nybble::from_u8(15).unwrap();
-        assert_eq!(nybble.to_u8(), 15);
-        assert_eq!(nybble.to_string(), "0x0F");
-        assert_eq!(Nybble::from_u8(15), Some(nybble));
-    }
-
-    #[test]
-    fn test_nybble_oob() {
-        assert_eq!(Nybble::from_u8(16), None);
+    fn test_from_u8_high_bits() {
+        let nybble = Nybble::from_u8(0b10101010);
+        assert_eq!(nybble.to_u8(), 0b1010);
     }
 
     #[test]
     fn test_get_bit() {
-        let nybble = Nybble::from_u8(12).unwrap();
+        let nybble = Nybble::from_u8(12);
         assert_eq!(nybble.get_bit(0), Bit::zero());
         assert_eq!(nybble.get_bit(1), Bit::zero());
         assert_eq!(nybble.get_bit(2), Bit::one());
@@ -526,7 +473,7 @@ mod tests {
     #[allow(unused_variables)]
     #[should_panic(expected = "Index out of bounds")]
     fn test_get_bit_oob() {
-        let nybble = Nybble::from_u8(12).unwrap();
+        let nybble = Nybble::from_u8(12);
         let p = nybble.get_bit(4);
     }
 
@@ -544,13 +491,13 @@ mod tests {
     #[test]
     #[should_panic(expected = "Index out of bounds")]
     fn test_set_bit_oob() {
-        let mut nybble = Nybble::from_u8(12).unwrap();
+        let mut nybble = Nybble::from_u8(12);
         nybble.set_bit(4);
     }
 
     #[test]
     fn test_unset_bit() {
-        let mut nybble = Nybble::from_u8(15).unwrap();
+        let mut nybble = Nybble::from_u8(15);
         nybble.unset_bit(0);
         nybble.unset_bit(1);
         nybble.unset_bit(2);
@@ -562,13 +509,13 @@ mod tests {
     #[test]
     #[should_panic(expected = "Index out of bounds")]
     fn test_unset_bit_oob() {
-        let mut nybble = Nybble::from_u8(12).unwrap();
+        let mut nybble = Nybble::from_u8(12);
         nybble.unset_bit(4);
     }
 
     #[test]
     fn test_flip_bit() {
-        let mut nybble = Nybble::from_u8(15).unwrap();
+        let mut nybble = Nybble::from_u8(15);
         nybble.flip_bit(0);
         nybble.flip_bit(1);
         nybble.flip_bit(2);
@@ -580,13 +527,13 @@ mod tests {
     #[test]
     #[should_panic(expected = "Index out of bounds")]
     fn test_flip_bit_oob() {
-        let mut nybble = Nybble::from_u8(12).unwrap();
+        let mut nybble = Nybble::from_u8(12);
         nybble.flip_bit(4);
     }
 
     #[test]
     fn test_flip() {
-        let mut nybble = Nybble::from_u8(15).unwrap();
+        let mut nybble = Nybble::from_u8(15);
         nybble.flip();
         assert_eq!(nybble.to_u8(), 0);
         assert_eq!(nybble.to_string(), "0x00");
@@ -594,9 +541,69 @@ mod tests {
 
     #[test]
     fn test_not() {
-        let nybble = Nybble::from_u8(15).unwrap();
+        let nybble = Nybble::from_u8(15);
         let nybble_not = !nybble;
         assert_eq!(nybble_not.to_u8(), 0);
         assert_eq!(nybble_not.to_string(), "0x00");
+    }
+
+    #[test]
+    fn test_and() {
+        let nybble_1 = Nybble::from_u8(0b1010);
+        let nybble_2 = Nybble::from_u8(0b1100);
+        let nybble_3 = nybble_1 & nybble_2;
+        assert_eq!(nybble_3.to_u8(), 0b1000);
+        assert_eq!(nybble_3.to_string(), "0x08");
+    }
+
+    #[test]
+    fn test_and_assign() {
+        let mut nybble_1 = Nybble::from_u8(0b1010);
+        let nybble_2 = Nybble::from_u8(0b1100);
+        nybble_1 &= nybble_2;
+        assert_eq!(nybble_1.to_u8(), 0b1000);
+        assert_eq!(nybble_1.to_string(), "0x08");
+    }
+
+    #[test]
+    fn test_or() {
+        let nybble_1 = Nybble::from_u8(0b1010);
+        let nybble_2 = Nybble::from_u8(0b1100);
+        let nybble_3 = nybble_1 | nybble_2;
+        assert_eq!(nybble_3.to_u8(), 0b1110);
+        assert_eq!(nybble_3.to_string(), "0x0E");
+    }
+
+    #[test]
+    fn test_or_assign() {
+        let mut nybble_1 = Nybble::from_u8(0b1010);
+        let nybble_2 = Nybble::from_u8(0b1100);
+        nybble_1 |= nybble_2;
+        assert_eq!(nybble_1.to_u8(), 0b1110);
+        assert_eq!(nybble_1.to_string(), "0x0E");
+    }
+
+    #[test]
+    fn test_xor() {
+        let nybble_1 = Nybble::from_u8(0b1010);
+        let nybble_2 = Nybble::from_u8(0b1100);
+        let nybble_3 = nybble_1 ^ nybble_2;
+        assert_eq!(nybble_3.to_u8(), 0b0110);
+        assert_eq!(nybble_3.to_string(), "0x06");
+    }
+
+    #[test]
+    fn test_xor_assign() {
+        let mut nybble_1 = Nybble::from_u8(0b1010);
+        let nybble_2 = Nybble::from_u8(0b1100);
+        nybble_1 ^= nybble_2;
+        assert_eq!(nybble_1.to_u8(), 0b0110);
+        assert_eq!(nybble_1.to_string(), "0x06");
+    }
+
+    #[test]
+    fn test_display() {
+        let nybble = Nybble::from_u8(10);
+        assert_eq!(format!("{}", nybble), "0x0A");
     }
 }
