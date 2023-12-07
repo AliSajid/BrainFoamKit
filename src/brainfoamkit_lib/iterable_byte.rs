1f3497fd1dbefdac69f1dd9ebb25c4e34c0d848e
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-12-07 13:02:06 -0500

    docs(IterableByte): add docs for the IterableByte struct

--- src/brainfoamkit_lib/iterable_byte.rs
+++ src/brainfoamkit_lib/iterable_byte.rs
@@ -45,12 +45,69 @@
 use crate::Bit;
 use crate::Byte;
 
+/// An iterator over a byte
+///
+/// This struct is a wrapper struct to generate an iterator
+/// for `Byte`. This allows us to map and/or loop over all the `Bit`s
+/// in the `Byte`.
+///
+/// # Examples
+///
+/// ```
+/// use brainfoamkit_lib::Bit;
+/// use brainfoamkit_lib::Byte;
+/// use brainfoamkit_lib::IterableByte;
+///
+/// let byte = Byte::from_u8(0b1100_1010); // Dec: 10; Hex: 0xA; Oct: 0o12
+/// let mut iter = IterableByte::new(&byte);
+///
+/// assert_eq!(iter.next(), Some(Bit::zero()));
+/// assert_eq!(iter.next(), Some(Bit::one()));
+/// assert_eq!(iter.next(), Some(Bit::zero()));
+/// assert_eq!(iter.next(), Some(Bit::one()));
+/// assert_eq!(iter.next(), Some(Bit::zero()));
+/// assert_eq!(iter.next(), Some(Bit::zero()));
+/// assert_eq!(iter.next(), Some(Bit::one()));
+/// assert_eq!(iter.next(), Some(Bit::one()));
+/// assert_eq!(iter.next(), None);
+/// ```
+///
+/// # See Also
+///
+/// * [`Byte`](crate::Byte)
+/// * [`Bit`](crate::Bit)
+/// * [`Nybble`](crate::Nybble)
+/// * [`IterableNybble`](crate::IterableNybble)
+///
 pub struct IterableByte<'a> {
     byte: &'a Byte,
     current_index: u8,
 }
 
 impl<'a> IterableByte<'a> {
+    /// Create a new `IterableByte` from a reference to a `Byte`
+    ///
+    /// # Arguments
+    ///
+    /// * `byte` - A reference to a `Byte` to iterate over
+    ///
+    /// # Returns
+    ///
+    /// A new `IterableNybble` that can be used to iterate over the `Nybble`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use brainfoamkit_lib::Byte;
+    /// use brainfoamkit_lib::IterableByte;
+    ///
+    /// let byte = Byte::from_u8(0b1100_1010); // Dec: 202; Hex: 0xCA; Oct: 0o312
+    /// let mut iter = IterableByte::new(&byte);
+    ///
+    /// for bit in iter {
+    ///    println!("{}", bit);
+    /// }
+    /// ```
     #[must_use]
     pub const fn new(byte: &'a Byte) -> Self {
         Self {
@@ -61,8 +118,36 @@ impl<'a> IterableByte<'a> {
 }
 
 impl<'a> Iterator for IterableByte<'a> {
+    /// The type of the element the iterator returns
     type Item = Bit;
 
+    /// Advance the iterator and return the next element
+    ///
+    /// # Returns
+    ///
+    /// The next element in the iterator
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use brainfoamkit_lib::Bit;
+    /// use brainfoamkit_lib::Byte;
+    /// use brainfoamkit_lib::IterableByte;
+    ///
+    /// let byte = Byte::from_u8(0b1100_1010); // Dec: 202; Hex: 0xCA; Oct: 0o312
+    ///
+    /// let mut iter = IterableByte::new(&byte);
+    ///
+    /// assert_eq!(iter.next(), Some(Bit::zero()));
+    /// assert_eq!(iter.next(), Some(Bit::one()));
+    /// assert_eq!(iter.next(), Some(Bit::zero()));
+    /// assert_eq!(iter.next(), Some(Bit::one()));
+    /// assert_eq!(iter.next(), Some(Bit::zero()));
+    /// assert_eq!(iter.next(), Some(Bit::zero()));
+    /// assert_eq!(iter.next(), Some(Bit::one()));
+    /// assert_eq!(iter.next(), Some(Bit::one()));
+    /// assert_eq!(iter.next(), None);
+    /// ```
     fn next(&mut self) -> Option<Self::Item> {
         let current_index = self.current_index;
         let next_index = current_index + 1;
